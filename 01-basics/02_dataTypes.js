28acc955de57d7523e9d50159a5b8b383e480eb7
Disha292 <127965049+Disha292@users.noreply.github.com>
2026-06-19 11:00:58 +0000

    datatypes done

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; //TREAT ALL JS CODE AS NEWER VERSION
+
+// alert("3 + 3"); // WE ARE USING NODE JS NOT BROWSER.
+
+console.log(3 + 3);
+console.log("Disha"); // CODE REDABILITY SHOULD BE HIGH.
+
+// DATATYPES
+
+// PRIMITIVE DATATYPES
+let name = "Disha Panchal"; //string
+let age = 23; //number
+let isLoggedIn = true; //boolean
+let state;
+
+// number (range ==> 2 to 2 to the power 53)
+// bigint
+// string (preferred in double quotes ==> "")
+// boolean (true/false or 1/0)
+// null (standalone value)
+// undefined (value is not assigned yet)
+// symbol (mostly used for uniqueness, espessially in React, figma etc)
+
+
+// NON- PRIMITIVE DATATYPES
+
+//objects
+
+console.log(typeof("disha"));
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
+
+
+
